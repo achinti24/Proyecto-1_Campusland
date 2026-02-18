ffda7ace401088782843ba4bb2a3714bd9c3d5f5
achinti24 <achintimorales@gmail.com>
2026-02-17 23:41:27 -0500

    se realizó ultimos ajustes al inicio de sesion de trainers y a los campers y sus rutas registradas en sus .json

--- main.py
+++ main.py
@@ -18,70 +18,65 @@ def cargar_campers():
     except FileNotFoundError:
         return []
 
+while True:
+    print("----------------------------------------")
+    print("   BIENVENIDO A CAMPUSLANDS")
+    print("----------------------------------------")
+    print("1. Iniciar sesión")
+    print("2. Registrarse como camper")
+    print("3. Salir")
+    print("----------------------------------------")
+    opcion = input("Seleccione una opción: ")
 
-def main():
-    while True:
-        print("\n----------------------------------------")
-        print("   BIENVENIDO A CAMPUSLANDS")
-        print("----------------------------------------")
-        print("1. Iniciar sesión")
-        print("2. Registrarse como camper")
-        print("3. Salir")
-        print("----------------------------------------")
+    if opcion == "1":
+        usuarios = cargar_usuarios()
+        datos_campers = cargar_campers()
 
-        opcion = input("Seleccione una opción: ")
+        correo = input("Correo electrónico: ")
+        password = input("Contraseña: ")
 
-        if opcion == "1":
-            usuarios = cargar_usuarios()
-            datos_campers = cargar_campers()
+        # iniciar sesion USUARIOS (COORDINADOR / TRAINER) 
+        usuario_encontrado = False
 
-            correo = input("Correo electrónico: ")
-            password = input("Contraseña: ")
+        for usuario in usuarios:
+            if usuario["correo"] == correo and usuario["password"] == password:
+                usuario_encontrado = True
+                print("Inicio de sesión exitoso")
+                if usuario["rol"] == "coordinador":
+                    nombre = usuario.get("nombre", "Coordinador")
+                    print(f"Bienvenido, {nombre}")
+                    menu_coordinador()
 
-            # ===== LOGIN USUARIOS (COORDINADOR / TRAINER) =====
-            usuario_encontrado = False
+                elif usuario["rol"] == "trainer":
+                    nombre_trainer = usuario.get("nombre")
+                    if not nombre_trainer:
+                        print("ERROR: Este usuario trainer no tiene nombre configurado")
+                        break
+                    print(f"Bienvenido, {nombre_trainer}")
+                    menu_trainer(nombre_trainer)
 
-            for usuario in usuarios:
-                if usuario["correo"] == correo and usuario["password"] == password:
-                    usuario_encontrado = True
-                    print("Inicio de sesión exitoso")
+                break
 
-                    if usuario["rol"] == "coordinador":
-                        menu_coordinador()
+        # iniciar sesion camper
+        camper_logueado = None
 
-                    elif usuario["rol"] == "trainer":
-                        nombre_trainer = usuario.get("nombre", "Trainer")
-                        menu_trainer(nombre_trainer)
+        for camper in datos_campers["campers"]:
+            if camper.get("correo") == correo and camper.get("password") == password:
+                camper_logueado = camper
+                break
 
-                    break
-
-            if usuario_encontrado:
-                continue
-
-            # ===== LOGIN CAMPER =====
-            camper_logueado = None
-
-            for camper in datos_campers["campers"]:
-                if camper.get("correo") == correo and camper.get("password") == password:
-                    camper_logueado = camper
-                    break
-
-            if camper_logueado:
-                print(f"\n Bienvenido {camper_logueado['nombre']} {camper_logueado['apellidos']}")
-                menu_camper(camper_logueado)
-            else:
-                print(" Correo o contraseña incorrectos")
-
-        elif opcion == "2":
-            registrar_camper()
-
-        elif opcion == "3":
-            print("Hasta luego ")
-            break
-
+        if camper_logueado:
+            print(f"Bienvenido {camper_logueado['nombre']} {camper_logueado['apellidos']}")
+            menu_camper(camper_logueado)
         else:
-            print("Opción inválida")
+            print(" Correo o contraseña incorrectos")
 
+    elif opcion == "2":
+        registrar_camper()
 
-if __name__ == "__main__":
-    main()
+    elif opcion == "3":
+        print("Hasta luego ")
+        break
+
+    else:
+        print("Opción inválida")
